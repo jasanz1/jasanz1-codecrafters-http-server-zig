e546f6f7f134d83dac9b76806a61d34fbe45d45b
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-14 19:14:18 -0800

    added 404 i think

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,20 @@
 const std = @import("std");
 const net = std.net;
+const response = @import("responses.zig");
 
+const ActiveRequest = struct {
+    method: []const u8,
+    path: []const u8,
+    version: []const u8,
+};
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
+    var activeRequest = ActiveRequest{
+        .method = "",
+        .path = "",
+        .version = "",
+    };
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     try stdout.print("Logs from your program will appear here!\n", .{});
 
@@ -15,7 +26,23 @@ pub fn main() !void {
     defer listener.deinit();
 
     const connection = try listener.accept();
-    try stdout.print("client connected!", .{});
-    try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
-    connection.stream.close();
+    defer connection.stream.close();
+    try stdout.print("client connected!\n", .{});
+    var buffer: [1024]u8 = undefined;
+    _ = try connection.stream.readAll(&buffer);
+    var request = std.mem.tokenizeSequence(u8, &buffer, "\r\n");
+    var requestLineToken = std.mem.tokenizeScalar(u8, request.next().?, ' ');
+    activeRequest.method = requestLineToken.next().?;
+    activeRequest.path = requestLineToken.next().?;
+    activeRequest.version = requestLineToken.next().?;
+
+    if (std.mem.eql(u8, activeRequest.path, "/")) {
+        try response.ok(connection);
+    } else {
+        try response.notFound(connection);
+    }
+
+    inline for (std.meta.fields(@TypeOf(activeRequest))) |f| {
+        std.log.debug(f.name ++ " {s}", .{@as(f.type, @field(activeRequest, f.name))});
+    }
 }

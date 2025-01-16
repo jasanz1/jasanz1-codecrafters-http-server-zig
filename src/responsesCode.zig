694d5171db6a6d3c3507418ac3076e84c2fde22c
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-16 14:08:12 -0800

    formatting......

--- src/responsesCode.zig
+++ src/responsesCode.zig
@@ -5,7 +5,7 @@ const stdout = std.io.getStdOut().writer();
 pub fn ok(header: []const u8, body: []const u8) ![]const u8 {
     try stdout.print("sending OK!\n", .{});
     var response = try append("HTTP/1.1 200 OK\r\n", header, " ");
-    response = try append(response, body, " ");
+    response = try append(response, body, "");
     response = try append(response, "\r\n", "");
     return response;
 }
@@ -13,7 +13,7 @@ pub fn ok(header: []const u8, body: []const u8) ![]const u8 {
 pub fn notFound(header: []const u8, body: []const u8) ![]const u8 {
     try stdout.print("sending Not Found!\n", .{});
     var response = try append("HTTP/1.1 404 Not Found\r\n", header, " ");
-    response = try append(response, body, " ");
+    response = try append(response, body, "");
     response = try append(response, "\r\n", "");
     return response;
 }

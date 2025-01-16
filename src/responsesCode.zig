3362e0742ed2ec42f51d0c77037f6c93b847fff4
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-16 14:20:36 -0800

    http is hard

--- src/responsesCode.zig
+++ src/responsesCode.zig
@@ -5,17 +5,15 @@ const stdout = std.io.getStdOut().writer();
 pub fn ok(header: []const u8, body: []const u8) ![]const u8 {
     try stdout.print("sending OK!\n", .{});
     var response = try append("HTTP/1.1 200 OK\r\n", header, "");
-    try stdout.print("http:\n {s}\n", .{response});
-    response = try append(response, body, "");
     response = try append(response, "\r\n", "");
+    response = try append(response, body, "");
     return response;
 }
 
 pub fn notFound(header: []const u8, body: []const u8) ![]const u8 {
     try stdout.print("sending Not Found!\n", .{});
     var response = try append("HTTP/1.1 404 Not Found\r\n", header, "");
-    try stdout.print("http:\n {s}\n", .{response});
-    response = try append(response, body, "");
     response = try append(response, "\r\n", "");
+    response = try append(response, body, "");
     return response;
 }

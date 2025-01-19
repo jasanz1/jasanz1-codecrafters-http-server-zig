8aef98c311a3accd39db045a7188418301ec67ec
Jacob Sanzone <jacobsanz1@gmail.com>
2025-01-18 17:48:53 -0800

    added generic base path functionality

--- src/responseBody.zig
+++ src/responseBody.zig
@@ -17,7 +17,14 @@ pub fn echo(path: *std.mem.TokenIterator(u8, .scalar)) []const u8 {
 
 pub fn files(path: *std.mem.TokenIterator(u8, .scalar)) error{FileNotFound}![]const u8 {
     const filePath = path.next().?;
-    const fullPath = append("/tmp/", filePath, "") catch "";
+    var basePath: ?[]const u8 = null;
+    var args = std.process.args();
+    while (args.next()) |arg| {
+        if (std.mem.eql(u8, arg, "--directory")) {
+            basePath = args.next().?;
+        }
+    }
+    const fullPath = append(basePath.?, filePath, "") catch "";
     var file = fs.cwd().openFile(fullPath, .{}) catch return error.FileNotFound;
     defer file.close();
     var buf_reader = std.io.bufferedReader(file.reader());
